6fb307fb3fb43d9aeed5de9bc4b932b5fd8c748c
Donis0215 <s2594603@gmail.com>
2026-01-23 15:22:26 +0800

    Update FPGA-Digital-Clock-Multiplexed.v

--- FPGA-Digital-Clock-Multiplexed.v
+++ FPGA-Digital-Clock-Multiplexed.v
@@ -13,12 +13,13 @@ assign led_ena_signal=1'b1;
 assign rst_hr=rst|(count5 == 2 && count4 == 3 && carry_min1 == 1);
 freq_div #21 slow_clk(clk,rst,clk_out);
 freq_div #15 scan_clk(clk, rst, clk_scan_to_light);
-counter sec0(clk_out,rst,ena,carry_sec0,light0,count0);
-counter #5 sec1(carry_sec0,rst,ena,carry_sec1,light1,count1);
-counter min0(carry_sec1,rst,ena,carry_min0,light2,count2);
-counter #5 min1(carry_min0,rst,ena,carry_min1,light3,count3);
-counter hr0(carry_min1,rst_hr,ena,carry_hr0,light4,count4);
-counter hr1(carry_hr0,rst_hr,ena,carry_hr1,light5,count5);
+// 所有人共用 clk_out
+counter     sec0(clk_out, rst, ena, carry_sec0, light0, count0);
+counter #5 sec1(clk_out, rst, carry_sec0, carry_sec1, light1, count1); // ena 接前一級的 carry
+counter     min0(clk_out, rst, carry_sec1, carry_min0, light2, count2);
+counter #5 min1(clk_out, rst, carry_min0, carry_min1, light3, count3);
+counter     hr0(carry_min1,rst_hr,ena,carry_hr0,light4,count4);
+counter     hr1(carry_hr0,rst_hr,ena,carry_hr1,light5,count5);
 always@(posedge clk_scan_to_light,posedge rst)begin
     if(rst)sel_num<=0;
     else if(sel_num==5)  sel_num<=0;
@@ -39,18 +40,18 @@ endmodule
 
 module counter(
 input clk,rst,ena,
-output reg carry,
+output  carry,
 output reg [6:0]light,
 output reg [3:0]count
 
 );
 parameter carry_base_minus_one=9;
-
+assign carry = (count == carry_base_minus_one) && (ena == 1);
 always@(posedge clk,posedge rst)begin
-    if(rst)begin count<=0;carry<=0;end
+    if(rst)begin count<=0;end
     else if(ena==1)begin
 	 if(count<carry_base_minus_one)count<=count+1;
-    else begin count<=0;carry<=carry+1'b1;end end end
+    else begin count<=0;end end end
     always@(*)begin
 case(count)
 4'b0000: light = 7'b1111110; // 0
